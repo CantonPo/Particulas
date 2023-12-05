0c943ef1065b720924669ff0f3f6435ecca5d945
CantonPo <cantoncorpas@gmail.com>
2023-12-05 14:44:43 +0100

    update2

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-let x
-let y
-let velX
-let velY
+class Particula{
+    constructor(){
+        this.x = width/2;
+        this.y = height/2;
+        this.velX = random(-1, 1);
+        this.velY = random(-1 ,1);
+        this.radius = random(20);
+    }
+    
+    show(){
+        
+        fill(178, 218, 250) 
+        ellipse(this.x, this.y, this.radius)
+    }
+    update(){
+        this.x += this.velX
+        this.y += this.velY
+    }
+    isDead(){
+        return (this.x > width || this.x < 0 ||this.y > height || this.y <0)
+    }
+}
+let p = []
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    console.log(width, height)
-    x = width/2
-    y = height/2
-    velX = -1
-    velY = -1
+    for(let i=0; i <= 2; i++){
+    p.push(new Particula())
+
+    }
   }
   
   function draw() {
     background(220);
-    ellipse(x, y, 50)
-    x += velX
-    y += velY
-    ellipse(0, 0, 50)
+    for(let i=0; i < p.length; i++){
+        p[i].update()
+        p[i].show()
+        if(p[i].isDead()){
+            p.splice(i, 1)
+            console.log("estoy cansado jefe")
+        }
+    
+        }
+    p.push(new Particula())
+    console.log(p.length)
   }
